5adb8e4590acd2c0f48e29bc871f3aab68690a68
SpeedKenode <88915329+SpeedKenode@users.noreply.github.com>
2023-10-18 17:30:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,60 +20,59 @@ for j in range(1, 34+1):
             text += "*"
         elif j==9 and (i==0):
             text += "*"
-        elif j==10 and ():
+        elif j==10 and ((i==1 or i==5) or (8<=i and i<=12) or (15<=i and i<=19) or (22<=i and i<=26) or (i==29 or i==33) or (37<=i and i<=39) or (i==43 or i==47) or (50<=i and i<= 54) or (58<=i and i<=61) or (64<=i and i<=68)):
             text += "*"
-        elif j==11 and ():
+        elif j==11 and ((i==1 or i==5) or (i==10) or (i==15) or (i==24) or (i==29 or i==33) or (i==36 or i==40) or (i==43 or i==47) or (i==50) or (i==57) or (i==64)):
             text += "*"
-        elif j==12 and ():
+        elif j==12 and ((i==1 or i==5) or (i==10) or (i==15) or (i==24) or (i==29 or i==30 or i==33) or (i==36 or i==40) or (i==43 or i==44 or i==46 or i==47) or (i==50) or (i==57) or (i==64)):
             text += "*"
-        elif j==13 and ():
+        elif j==13 and ((i==1 or i==5) or (i==10) or (15<=i and i<=18) or (i==24) or (i==29 or i==31 or i==33) or (36<=i and i<=40) or (i==43 or i==45 or i==47) or (50<=i and i<=54) or (58<=i and i<=60) or (64<=i and i<=67)):
             text += "*"
-        elif j==14 and ():
+        elif j==14 and ((i==1 or i==5) or (i==10) or (i==15) or (i==24) or (i==36 or i==40) or (i==43 or i==47) or (i==29 or i==31 or i==33) or (i==50) or (i==61) or (i==64)):
             text += "*"
-        elif j==15 and ():
+        elif j==15 and ((i==2 or i==4) or (i==10) or (i==15) or (i==24) or (i==29 or i==33) or (i==36 or i==40) or (i==43 or i==47) or (i==50) or (i==61) or (i==64)):
             text += "*"
-        elif j==16 and ():
+        elif j==16 and ((i==3) or (8<=i and i<=12) or (15<=i and i<=19) or (i==24) or (i==29 or i==33) or (i==36 or i==40) or (i==43 or i==47) or (50<=i and i<=54) or (57<=i and i<=60) or (64<=i and i<=68)):
             text += "*"
         elif j==17 and (i==0):
             text += "*"
         elif j==18 and (i==0):
             text += "*"
-        elif j==19 and ((i==20 or i==24)  or (28<=i and i<=30) or (i==34 or i==38)):
+        elif j==19 and ((i==20 or i==24)  or (28<=i and i<=30) or (i==34 or i==38) or (41<=i and i<=45) or (i==48 or i==52)):
             text += "*"
-        elif j==20 and ((i==20 or i==24)  or (i==27 or i==31) or (i==34 or i==38)):
+        elif j==20 and ((i==20 or i==24)  or (i==27 or i==31) or (i==34 or i==38) or (i==41) or (i==48 or i==52)):
             text += "*"
-        elif j==21 and ((i==20 or i==24)  or (i==27 or i==31)):
+        elif j==21 and ((i==20 or i==24)  or (i==27 or i==31) or (i==34 or i==35 or i==37 or i==38) or (i==41) or (i==48 or i==49 or i==52)):
             text += "*"
-        elif j==22 and ((i==20 or i==24)  or (i==27 or i==31) or i==36):
+        elif j==22 and ((i==20 or i==24)  or (i==27 or i==31) or (i==34 or i==36 or i==38) or (41<=i and i<=44) or (i==48 or i==50 or i==52)):
             text += "*"
-        elif j==23 and ((i==20 or i==22 or i==24)  or (i==27 or i==31) or (i==34 or i==38)):
+        elif j==23 and ((i==20 or i==22 or i==24)  or (i==27 or i==31) or (i==34 or i==38) or (i==41) or (i==48 or i==51 or i==52)):
             text += "*"
-        elif j==24 and ((i==20 or i==22 or i==24)  or (i==27 or i==31) or (i==34 or i==38)):
+        elif j==24 and ((i==20 or i==22 or i==24)  or (i==27 or i==31) or (i==34 or i==38) or (i==41) or (i==48 or i==52)):
             text += "*"
-        elif j==25 and ((i==21 or i==23)  or (28<=i and i<=30) or (i==34 or i==38)):
+        elif j==25 and ((i==21 or i==23)  or (28<=i and i<=30) or (i==34 or i==38) or (41<=i and i<=45) or (i==48 or i==52)):
             text += "*"
         elif j==26 and (i==0):
             text += "*"
         elif j==27 and (i==0):
             text += "*"
-        elif j==28 and ((30<=i and i<=33) or (38<=i and i<=40) or (i==44 or i==48)):
+        elif j==28 and ((26<=i and i<=29) or (34<=i and i<=36) or (i==40 or i==44)):
             text += "*"
-        elif j==29 and ((i==30 or i==34) or (i==37 or i==41) or (i==44 or i==48)):
+        elif j==29 and ((i==26 or i==30) or (i==33 or i==37) or (i==40 or i==44)):
             text += "*"
-        elif j==30 and ((i==30 or i==34) or (i==37 or i==41) or (i==45 or i==47)):
+        elif j==30 and ((i==26 or i==30) or (i==33 or i==37) or (i==41 or i==43)):
             text += "*"
-        elif j==31 and ((i==30 or i==34) or (37<=i and i<=41) or i==46):
+        elif j==31 and ((i==26 or i==30) or (33<=i and i<=37) or i==42):
             text += "*"
-        elif j==32 and ((i==30 or i==34) or (i==37 or i==41) or i==46):
+        elif j==32 and ((i==26 or i==30) or (i==33 or i==37) or i==42):
             text += "*"
-        elif j==33 and ((i==30 or i==34) or (i==37 or i==41) or i==46):
+        elif j==33 and ((i==26 or i==30) or (i==33 or i==37) or i==42):
             text += "*"
-        elif j==34 and ((30<=i and i<=33) or (i==37 or i==41) or i==46):
+        elif j==34 and ((26<=i and i<=29) or (i==33 or i==37) or i==42):
             text += "*"
         else:
             text += " "
             
     text += "\n"
-
 
 print(text)
