471d0fd309f823985cb10ddc42d9578a1a10b71e
SpeedKenode <88915329+SpeedKenode@users.noreply.github.com>
2023-10-18 21:54:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ for j in range(1, 9+1):
     for i in range(1, 68+1):
         if j==1 and ((20<= i and i<=22) or (27<= i and i<=29) or i==40 or (46<=i and i<= 48)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==2 and ((i==19 or i==23) or (i==26 or i==30) or (i==39 or i==40) or (i==45 or i==49)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==3 and (i==23 or (i==26 or i==30) or i==40 or (i==45 or i==49)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==4 and (i==22 or (i==26 or i==30) or (33<=i and i<=36) or i==40 or (i==45 or i==49)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==5 and (i==21 or (i==26 or i==30) or i==40 or (i==45 or i==49)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==6 and ((i==19 or i==20) or(i==26 or i==30) or i==40 or (i==45 or i==49)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==7 and ((19<=i and i<=23) or (27<= i and i<=29) or (39<=i and i<=41) or (46<=i and i<= 48)):
             num += "*"
+			time.sleep(0.5)
         
         elif j==8 and ():
             num += "*"
@@ -44,72 +51,100 @@ for j in range(34):
     for i in range(68):
         if j==0 and ((i==19 or i==23) or (27<=i and i<=29) or (33<=i and i<=36) or (40<=i and i<=43) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==1 and ((i==19 or i==23) or (i==26 or i==30) or (i==33 or i==37) or (i==40 or i==44) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==2 and ((i==19 or i==23) or (i==26 or i==30) or (i==33 or i==37) or (i==40 or i==44) or (i==48 or i==50)):
             text += "*"
+			time.sleep(0.5)
         elif j==3 and ((19<=i and i<=23) or (26<=i and i<=30) or (33<=i and i<=36) or (40<=i and i<=43) or i==49):
             text += "*"
+			time.sleep(0.5)
         elif j==4 and ((i==19 or i==23) or (i==26 or i==30) or i==33 or i==40 or i==49):
             text += "*"
+			time.sleep(0.5)
         elif j==5 and ((i==19 or i==23) or (i==26 or i==30) or i==33 or i==40 or i==49):
             text += "*"
+			time.sleep(0.5)
         elif j==6 and ((i==19 or i==23) or (i==26 or i==30) or i==33 or i==40 or i==49):
             text += "*"
+			time.sleep(0.5)
         elif j==7 and (i==-1):
             text += "*"
         elif j==8 and (i==-1):
             text += "*"
         elif j==9 and ((i==0 or i==4) or (7<=i and i<=11) or (14<=i and i<=18) or (21<=i and i<=25) or (i==28 or i==32) or (36<=i and i<=38) or (i==42 or i==46) or (49<=i and i<= 53) or (57<=i and i<=60) or (63<=i and i<=67)):
             text += "*"
+			time.sleep(0.5)
         elif j==10 and ((i==0 or i==4) or (i==9) or (i==14) or (i==23) or (i==28 or i==32) or (i==35 or i==39) or (i==42 or i==46) or (i==49) or (i==56) or (i==63)):
             text += "*"
+			time.sleep(0.5)
         elif j==11 and ((i==0 or i==4) or (i==9) or (i==14) or (i==23) or (i==28 or i==29 or i==31 or i==32) or (i==35 or i==39) or (i==42 or i==43 or i==45 or i==46) or (i==49) or (i==56) or (i==63)):
             text += "*"
+			time.sleep(0.5)
         elif j==12 and ((i==0 or i==4) or (i==9) or (14<=i and i<=17) or (i==23) or (i==28 or i==30 or i==32) or (35<=i and i<=39) or (i==42 or i==44 or i==46) or (49<=i and i<=52) or (57<=i and i<=59) or (63<=i and i<=66)):
             text += "*"
+			time.sleep(0.5)
         elif j==13 and ((i==0 or i==4) or (i==9) or (i==14) or (i==23) or (i==35 or i==39) or (i==42 or i==46) or (i==28 or i==32) or (i==49) or (i==60) or (i==63)):
             text += "*"
+			time.sleep(0.5)
         elif j==14 and ((i==1 or i==3) or (i==9) or (i==14) or (i==23) or (i==28 or i==32) or (i==35 or i==39) or (i==42 or i==46) or (i==49) or (i==60) or (i==63)):
             text += "*"
+			time.sleep(0.5)
         elif j==15 and ((i==2) or (7<=i and i<=11) or (14<=i and i<=18) or (i==23) or (i==28 or i==32) or (i==35 or i==39) or (i==42 or i==46) or (49<=i and i<=53) or (56<=i and i<=59) or (63<=i and i<=67)):
             text += "*"
+			time.sleep(0.5)
         elif j==16 and (i==-1):
             text += "*"
         elif j==17 and (i==-1):
             text += "*"
         elif j==18 and ((i==19 or i==23)  or (27<=i and i<=29) or (i==33 or i==37) or (40<=i and i<=44) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==19 and ((i==19 or i==23)  or (i==26 or i==30) or (i==33 or i==37) or (i==40) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==20 and ((i==19 or i==23)  or (i==26 or i==30) or (i==33 or i==34 or i==36 or i==37) or (i==40) or (i==47 or i==48 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==21 and ((i==19 or i==23)  or (i==26 or i==30) or (i==33 or i==35 or i==37) or (40<=i and i<=43) or (i==47 or i==49 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==22 and ((i==19 or i==21 or i==23)  or (i==26 or i==30) or (i==33 or i==37) or (i==40) or (i==47 or i==50 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==23 and ((i==19 or i==21 or i==23)  or (i==26 or i==30) or (i==33 or i==37) or (i==40) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==24 and ((i==20 or i==22)  or (27<=i and i<=29) or (i==33 or i==37) or (40<=i and i<=44) or (i==47 or i==51)):
             text += "*"
+			time.sleep(0.5)
         elif j==25 and (i==-1):
             text += "*"
         elif j==26 and (i==-1):
             text += "*"
         elif j==27 and ((25<=i and i<=28) or (33<=i and i<=35) or (i==39 or i==43)):
             text += "*"
+			time.sleep(0.5)
         elif j==28 and ((i==25 or i==29) or (i==32 or i==36) or (i==39 or i==43)):
             text += "*"
+			time.sleep(0.5)
         elif j==29 and ((i==25 or i==29) or (i==32 or i==36) or (i==40 or i==42)):
             text += "*"
+			time.sleep(0.5)
         elif j==30 and ((i==25 or i==29) or (32<=i and i<=36) or i==41):
             text += "*"
+			time.sleep(0.5)
         elif j==31 and ((i==25 or i==29) or (i==32 or i==36) or i==41):
             text += "*"
+			time.sleep(0.5)
         elif j==32 and ((i==25 or i==29) or (i==32 or i==36) or i==41):
             text += "*"
+			time.sleep(0.5)
         elif j==33 and ((25<=i and i<=28) or (i==32 or i==36) or i==41):
             text += "*"
+			time.sleep(0.5)
         else:
             text += " "
             
@@ -126,14 +161,17 @@ for i in range(n//2-1):
 		
 		if i == n//2-2 and (j == 0 or j == m-1):
 			print("*", end=" ")
+			time.sleep(0.5)
 			
 		elif j <= m//2 and ((i+j == n//2-3 and j <= m//4) \
 							or (j-i == m//2-n//2+3 and j > m//4)):
 			print("*", end=" ")
+			time.sleep(0.5)
 			
 		elif j > m//2 and ((i+j == n//2-3+m//2 and j < 3*m//4) \
 						or (j-i == m//2-n//2+3+m//2 and j >= 3*m//4)):
 			print("*", end=" ")
+			time.sleep(0.5)
 
 		else:
 			print(" ", end=" ")
@@ -144,6 +182,7 @@ for i in range(n//2-1, n):
 		
 		if (i-j == n//2-1) or (i+j == n-1+m//2):
 			print('*', end=" ")
+			time.sleep(0.5)
 
 		else:
 			print(' ', end=" ")
